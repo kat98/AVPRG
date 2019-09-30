d9424b78c77488c1d1d2c9bee753c3d86dc2dc4d
katjaschottler <katja.schoettler@gmail.com>
2019-09-30 09:38:26 +0200

    ss

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-
-
 document.body.addEventListener('mousemove', function(e){
-    console.log(e.clientX);
-    console.log(e.clientY);
+   console.log(e.clientX);
+   console.log(e.clientY);
 
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
+   console.log(window.innerWidth);
+  console.log(window.innerHeight);
+    var frequency = getFrequency(e.clientX)
+
+    var context = new AudioContext();
+    var oscillatorNode = context.createOscillator();
+    var gainNode = context.createGain();
+        
+    oscillatorNode.connect(gainNode);
+    gainNode.connect(context.destination);
+
+    gainNode.gain.value = (e.clientY/100);
+    oscillatorNode.frequency.value = frequency;
+
+    oscillatorNode.start(context.currentTime);
+    oscillatorNode.stop(context.currentTime + 1);
+
 });
+ function getFrequency(value){
+                
+                for (let index = 0; index < allFrequencies.length; index++) {
+                    const element = allFrequencies[value];
+                   return element
+       
+                }
+            }
 
-var context = new AudioContext();
-var oscillatorNode = context.createOscillator();
-var gainNode = context.createGain();
-    
-oscillatorNode.connect(gainNode);
-gainNode.connect(context.destination);
-
-gainNode.gain.value = 0.3;
-oscillatorNode.frequency.value = 880;
-
-oscillatorNode.start(context.currentTime);
-oscillatorNode.stop(context.currentTime + 1);
