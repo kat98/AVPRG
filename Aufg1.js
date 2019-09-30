945b1f97e15e01aa65ab259f77b5165022536669
katjaschottler <katja.schoettler@gmail.com>
2019-09-30 09:26:07 +0200

    shd

---
+++ Aufg1.js
@@ -0,0 +1,6 @@
+//war vorher script.js
+
+document.getElementById("btn").addEventListener("click", function(){
+    var x =document.getElementById("display").value
+    calculate(x)
+})
--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-document.getElementById("btn").addEventListener("click", function(){
-    var x =document.getElementById("display").value
-    calculate(x)
-})
 
-function calculate(value){
 
-}
+document.body.addEventListener('mousemove', function(e){
+    console.log(e.clientX);
+    console.log(e.clientY);
+
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+});
+
+var context = new AudioContext();
+var oscillatorNode = context.createOscillator();
+var gainNode = context.createGain();
+    
+oscillatorNode.connect(gainNode);
+gainNode.connect(context.destination);
+
+gainNode.gain.value = 0.3;
+oscillatorNode.frequency.value = 880;
+
+oscillatorNode.start(context.currentTime);
+oscillatorNode.stop(context.currentTime + 1);
